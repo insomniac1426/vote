9f71cce097e0f72d2cc8be2b4f0a91d665c53e8b
insomniac1426 <yashbansal14296@gmail.com>
2018-12-28 12:52:31 +0530

    created basic react app

---
+++ ReactApp/src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css'
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Vote</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
